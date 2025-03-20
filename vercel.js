56eca645795d61c2b39060aa75de399835923dac
spac-null <dev@nullspace.it>
2025-03-20 21:07:51 +0100

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,15 @@
+{
+  "redirects": [
+    {
+      "source": "/",
+      "has": [
+        {
+          "type": "host",
+          "value": "qr.altrospazio.org"
+        }
+      ],
+      "destination": "/menu-gruppo-nazario.pdf",
+      "permanent": true
+    }
+  ]
+}
